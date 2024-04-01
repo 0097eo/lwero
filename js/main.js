a60ad43b6fe9665c86f1c23a421ac5dcd5092b86
Emmanuel <emmanuelokello294@gmail.com>
2024-04-01 09:12:20 +0300

    updates linkedin and email links

--- js/main.js
+++ js/main.js
@@ -70,3 +70,23 @@
         return false;
     });
 })(jQuery);
+
+//Download Resume
+function downloadResume() {
+    const resumeUrl = "Files/reume.pdf";
+
+    const fileName = "VincentLwero_Resume.pdf";
+  
+    fetch(resumeUrl)
+      .then(response => response.blob())  
+      .then(blob => {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        link.click();
+        URL.revokeObjectURL(link.href);
+      })
+
+  }
+  
+  document.getElementById('downloadResume').addEventListener('click', downloadResume);
